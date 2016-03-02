fc330561910f6aa36a67f5697991749e1d440da5
BuildTools <thesettler2@hotmail.com>
2016-03-02 22:21:07 +0800

    Update 2.4.0.2
    Update 2.4.0.2

--- Configuration.cs
+++ Configuration.cs
@@ -19,10 +19,34 @@ namespace LIGHT
         public bool LPXEnabled;
         public bool IncomeEnabled;
         public bool KitsEnabled;
+        public bool KitCooldownApplyToAll;
+        public bool KitCooldownResetOnDeath;
         public ushort IncomeInterval;        
         public bool AutoRemoveEnabled;        
         public int AutoRemoveDays;
         public bool AutoAddDefault;
+        public string DatabaseCarOwners;
+        public bool AllowCarOwnerShip;
+        public bool DriveUnownedCar;
+        public double LicencePrice;
+        public double FuelPrice;
+        public double RepairPrice;
+        public bool CanBuyItems;
+        public bool CanBuyVehicles;
+        public bool CanSellItems;
+        public bool QualityCounts;
+        public bool EnableShop;
+        public bool EnableAutoDBUpdate;
+        public string DatabaseItemShop;
+        public string DatabaseVehicleShop; 
+        public bool SaleEnable;
+        public int SalePercentage;
+        public int minNextSaleTime;
+        public int maxNextSaleTime;
+        public int SaleTime;
+        public bool AllowAuction;
+        public string DatabaseAuction;
+        
 
         public void LoadDefaults()
         {
@@ -37,10 +61,33 @@ namespace LIGHT
             LPXEnabled = true;
             IncomeEnabled = true;
             KitsEnabled = false;
+            KitCooldownApplyToAll = true;
+            KitCooldownResetOnDeath = false;
             IncomeInterval = 900;
             AutoRemoveEnabled = false;
             AutoRemoveDays = 30;
             AutoAddDefault = false;
+            DatabaseCarOwners = "Car";
+            AllowCarOwnerShip = true;
+            DriveUnownedCar = false;
+            LicencePrice = 100.00;
+            FuelPrice = 1.3;
+            RepairPrice = 2;
+            CanBuyItems = true;
+            CanBuyVehicles = false;
+            CanSellItems = true;
+            QualityCounts = true;
+            EnableShop = true;
+            EnableAutoDBUpdate = false;
+            DatabaseItemShop = "ItemShop";
+            DatabaseVehicleShop = "VehicleShop";
+            SaleEnable = false;
+            SalePercentage = 15;
+            minNextSaleTime = 600;
+            maxNextSaleTime = 1200;
+            SaleTime = 3;
+            AllowAuction = false;
+            DatabaseAuction = "AuctionShop";
         }
     }
 }
